3667142a71b69688e1d402206ad0a8719464396d
Arthur Bousquet <arth.bousquet@gmail.com>
2018-01-23 10:47:19 -0500

    improved the speed

--- Examples.py
+++ Examples.py
@@ -1,19 +1,10 @@
 #! /usr/bin/python2.7
-
 """
 Non convex example
 """
 
-#from __future__ import print_function
 import numpy as np
-import matplotlib.pyplot as plt
-import matplotlib.tri as tri
-import numpy.random as rd
-import os
-import sys
 from StoDescLib import *
-
-
 
 def f(xt):
     return 0.5*xt[0]**2.0+0.5*(1.0-np.cos(2*xt[0]))+xt[1]**2.0
@@ -24,15 +15,16 @@ K=10
 itmax=1000
 tol=1E-5
 lda=[0.1,0.1,0.1]
+gamma = 1.0
+amp = 1.0
 x0 = np.array([3.0,-5.0])
 v0 = np.copy(x0)
 z0 = np.copy(x0)
 
+bounds=[-5.0,5.0]
 
-gamma = 1.0
-amp = 1.0
 [x1,min1,it1] = GradientNoise(f,df,gamma,amp,tol,x0,itmax,K)
-[x2,v2,min2,it2] = Momentum1(f,df,lda[0],gamma,amp,tol,x0,v0,itmax,K)
+[x2,v2,min2,it2] = Momentum1(f,df,lda[0],gamma,amp,tol,x0,v0,itmax,K,bounds=bounds)
 [x3,v3,z3,min3,it3] = Momentum2(f,df,lda,gamma,amp,tol,x0,v0,z0,itmax,K)
 
 
--- StoDescLib.py
+++ StoDescLib.py
@@ -3,8 +3,8 @@
 """
 Different Descent Algorithms
  * Gradient Descent with noise
- * Moment 1 with noise
- * Moment 2 with noise
+ * Momentum 1 with noise
+ * Momentum 2 with noise
 """
 
 import warnings
@@ -14,31 +14,54 @@ import os
 import sys
 from scipy.optimize import *
 
-# Moment 2 Algorithm
-# INPUT:
-#   * Func = Function to minimize
-#   * Jac  = Jacobian of the function to minimize
-#   * lda = vector of dimension 3 where each value is  between 0 and 1
-#   * gamma =  Real number that divide the gradient
-#   * amp=  Real number, amplitude of the noise
-#   * tol =  Real number, tolerance for the convergence
-#   * x_init  = Initial guess of x
-#   * v_init  = Initial guess for moment 1 (velocity)
-#   * z_init  = Initial guess for moment 2
-#   * itmax  = Interger, maximum number of iteration
-#   * K  = Interger, number of times to repeat the algorithm
-#   * it_init (optional) = Integer, iteration number where to start the algorithm
-#   * bounds (optional) = Real of dimension 2 where each value of x has to be between
-#   * DirDoF (optional) = vector, degree(s) of freedom where x will not change
-# OUPUT:
-#   * x  = value of x where Func(x) is the minimum
-#   * v  = value of moment 1 (velocity)
-#   * z = value ofmoment 2
-#   * min_Func = value of the minimum of Func(x)
-#   * min_it = iteration number where the algorithm stopped
-def Moment2(Func,Jac,lda,gamma,amp,tol,x_init,v_init,z_init, \
+"""
+Momentum 2 Algorithm
+PARAMETERS:
+-----------
+   * Func : callable
+            Function to minimize
+   * Jac  : callable
+            Jacobian of the function to minimize
+   * lda : ndarray
+            vector of dimension 3 where each value is  between 0 and 1
+   * gamma : float
+            Real number that divide the gradient
+   * amp :  float
+            Real number, amplitude of the noise
+   * tol :  flaot
+            Real number, tolerance for the convergence
+   * x_init  : ndarray
+            Initial guess of x
+   * v_init  : ndarray
+            Initial guess for Momentum 1 (velocity), same shape as x0
+   * z_init : ndarray
+            Initial guess for Momentum 2, same shape as x0
+   * itmax  : int
+            maximum number of iteration
+   * K  : int
+            number of times to repeat the algorithm
+   * it_init : int, optional
+            iteration number where to start the algorithm
+   * bounds : sequence
+             dimension 2 where each value of x has to be between
+   * DirDoF : list of tuple
+            degree(s) of freedom where x will not change
+ RETURNS:
+ -------
+   * x  : ndarray
+        value of x where Func(x) is the minimum, same size of x_init
+   * v  : ndarray
+        value of Momentum 1 (velocity)
+   * z : ndarray
+        value ofMomentum 2
+   * min_Func : flaot
+        value of the minimum of Func(x)
+   * min_it : int
+        iteration number where the algorithm stopped
+"""
+def Momentum2(Func,Jac,lda,gamma,amp,tol,x_init,v_init,z_init, \
             itmax,K,it_init=None,bounds=None,DirDoF=None):
-    print "Moment 2..."
+    print "Momentum 2..."
     N = len(x_init)
     min_x = np.copy(x_init)
     min_v = np.copy(v_init)
@@ -54,10 +77,9 @@ def Moment2(Func,Jac,lda,gamma,amp,tol,x_init,v_init,z_init, \
 
     min_it=it_init
 
-    DoF = range(N)
+    # Save the DOF
     if DirDoF!=None:
-        for i in DirDoF:
-            DoF.remove(i)
+        SavedDOF = x[DirDoF]
 
     print "\t Initial Gradient = ", np.linalg.norm(Jac(x_init)), "Initial Energy = ", min_Func
     for j in range(K):
@@ -69,18 +91,18 @@ def Moment2(Func,Jac,lda,gamma,amp,tol,x_init,v_init,z_init, \
         while ( (it < itmax) ):
             dt = 1.0/np.log(float(it+2))**0.5
             sigma = amp*np.sqrt(gamma/(np.log(it+2)*dt))*1.0/float(it+2)
-            z = (1.0 -B*dt)*z + C*v*dt + sigma*rd.normal(0.0,1.0,N)*np.sqrt(dt)
+            z = (1.0 -B*dt)*z + C*v*dt + sigma*rd.normal(0.0,1.0,x.shape)*np.sqrt(dt)
             b = Jac(x)
             v = (1.0-mu*dt)*v - dt*b/gamma - z*dt
             new_x = x + v*dt
 
             if (bounds!=None and len(bounds)==2 ):
-                for i in DoF:
-                    if (new_x[i]>bounds[0] and new_x[i]<bounds[1]):
-                        # print i, it, new_x[i], b[i], z[i], v[i], sigma, dt
-                        x[i]=new_x[i]
+                choice = [new_x>bounds[0],new_x<bounds[1]]
+                x=np.select(choice,[new_x,x])
             else:
-                x = np.copy(new_x)
+                x = new_x
+            if DirDoF!=None:
+                x[DirDoF]=SavedDOF
 
             it+=1
             PrevEnergy = Energy
@@ -102,30 +124,51 @@ def Moment2(Func,Jac,lda,gamma,amp,tol,x_init,v_init,z_init, \
 
     return [min_x,min_v,min_z,min_Func,min_it]
 
-# Moment 1 Algorithm
-# INPUT:
-#   * Func = Function to minimize
-#   * Jac  = Jacobian of the function to minimize
-#   * lda = real number between 0 and 1
-#   * gamma =  Real number that divide the gradient
-#   * amp=  Real number, amplitude of the noise
-#   * tol =  Real number, tolerance for the convergence
-#   * x_init  = Initial guess of x
-#   * v_init  = Initial guess for moment 1 (velocity)
-#   * itmax  = Interger, maximum number of iteration
-#   * K  = Interger, number of times to repeat the algorithm
-#   * it_init (optional) = Integer, iteration number where to start the algorithm
-#   * bounds (optional) = Real of dimension 2 where each value of x has to be between
-#   * DirDoF (optional) = vector, degree(s) of freedom where x will not change
-# OUPUT:
-#   * x  = value of x where Func(x) is the minimum
-#   * v  = value of moment 1 (velocity)
-#   * min_Func = value of the minimum of Func(x)
-#   * min_it = iteration number where the algorithm stopped
-def Moment1(Func,Jac,lda,gamma,amp,tol,x_init,v_init,\
+"""
+Momentum 1 Algorithm
+PARAMETERS:
+-----------
+   * Func : callable
+            Function to minimize
+   * Jac  : callable
+            Jacobian of the function to minimize
+   * lda : float
+            value is between 0 and 1
+   * gamma : float
+            Real number that divide the gradient
+   * amp :  float
+            Real number, amplitude of the noise
+   * tol :  flaot
+            Real number, tolerance for the convergence
+   * x_init  : ndarray
+            Initial guess of x
+   * v_init  : ndarray
+            Initial guess for Momentum 1 (velocity), same shape as x0
+   * itmax  : int
+            maximum number of iteration
+   * K  : int
+            number of times to repeat the algorithm
+   * it_init : int, optional
+            iteration number where to start the algorithm
+   * bounds : sequence
+             dimension 2 where each value of x has to be between
+   * DirDoF : list of tuple
+            degree(s) of freedom where x will not change
+ RETURNS:
+ -------
+   * x  : ndarray
+        value of x where Func(x) is the minimum, same size of x_init
+   * v  : ndarray
+        value of Momentum 1 (velocity)
+   * min_Func : flaot
+        value of the minimum of Func(x)
+   * min_it : int
+        iteration number where the algorithm stopped
+"""
+def Momentum1(Func,Jac,lda,gamma,amp,tol,x_init,v_init,\
         itmax,K,it_init=None,bounds=None,DirDoF=None):
 
-    print "Moment 1..."
+    print "Momentum 1..."
     N = len(x_init)
     min_x = np.copy(x_init)
     min_v = np.copy(v_init)
@@ -137,10 +180,9 @@ def Moment1(Func,Jac,lda,gamma,amp,tol,x_init,v_init,\
 
     min_it=it_init
 
-    DoF = range(N)
+    # Save the DOF
     if DirDoF!=None:
-        for i in DirDoF:
-            DoF.remove(i)
+        SavedDOF = x[DirDoF]
 
     print "Initial Gradient = ", np.linalg.norm(Jac(x_init)), "Initial Energy = ", min_Func
     for j in range(K):
@@ -152,15 +194,16 @@ def Moment1(Func,Jac,lda,gamma,amp,tol,x_init,v_init,\
             dt = 1.0/np.log(float(it+2))**0.5
             sigma = amp*np.sqrt(gamma/(np.log(it+2)*dt))*1.0/float(it+2)
             b = Jac(x)
-            v = (1.0-lda*dt)*v - dt*b/gamma + sigma*np.sqrt(dt)/gamma*rd.normal(0.0,1.0,N)
+            v = (1.0-lda*dt)*v - dt*b/gamma + sigma*np.sqrt(dt)/gamma*rd.normal(0.0,1.0,x.shape)
             new_x = x + v*dt
 
             if (bounds!=None and len(bounds)==2 ):
-                for i in DoF:
-                    if (new_x[i]>bounds[0] and new_x[i]<bounds[1]):
-                        x[i]=new_x[i]
+                choice = [new_x>bounds[0],new_x<bounds[1]]
+                x=np.select(choice,[new_x,x])
             else:
-                x = np.copy(new_x)
+                x = new_x
+            if DirDoF!=None:
+                x[DirDoF]=SavedDOF
 
             it+=1
             PrevEnergy = Energy
@@ -180,23 +223,41 @@ def Moment1(Func,Jac,lda,gamma,amp,tol,x_init,v_init,\
     return [min_x,min_v,min_Func,min_it]
 
 
-# Gradient descent with Noise Algorithm
-# INPUT:
-#   * Func = Function to minimize
-#   * Jac  = Jacobian of the function to minimize
-#   * gamma =  Real number that divide the gradient
-#   * amp=  Real number, amplitude of the noise
-#   * tol =  Real number, tolerance for the convergence
-#   * x_init  = Initial guess of x
-#   * itmax  = Interger, maximum number of iteration
-#   * K  = Interger, number of times to repeat the algorithm
-#   * it_init (optional) = Integer, iteration number where to start the algorithm
-#   * bounds (optional) = Real of dimension 2 where each value of x has to be between
-#   * DirDoF (optional) = vector, degree(s) of freedom where x will not change
-# OUPUT:
-#   * x  = value of x where Func(x) is the minimum
-#   * min_Func = value of the minimum of Func(x)
-#   * min_it = iteration number where the algorithm stopped
+"""
+Gradient Descent with Noise Algorithm
+PARAMETERS:
+-----------
+   * Func : callable
+            Function to minimize
+   * Jac  : callable
+            Jacobian of the function to minimize
+   * gamma : float
+            Real number that divide the gradient
+   * amp :  float
+            Real number, amplitude of the noise
+   * tol :  flaot
+            Real number, tolerance for the convergence
+   * x_init  : ndarray
+            Initial guess of x
+   * itmax  : int
+            maximum number of iteration
+   * K  : int
+            number of times to repeat the algorithm
+   * it_init : int, optional
+            iteration number where to start the algorithm
+   * bounds : sequence
+             dimension 2 where each value of x has to be between
+   * DirDoF : list of tuple
+            degree(s) of freedom where x will not change
+ RETURNS:
+ -------
+   * x  : ndarray
+        value of x where Func(x) is the minimum, same size of x_init
+   * min_Func : flaot
+        value of the minimum of Func(x)
+   * min_it : int
+        iteration number where the algorithm stopped
+"""
 def GradientNoise(Func,Jac,gamma,amp,tol,x_init,itmax,K,it_init=None, \
         bounds=None,DirDoF=None):
     print "Gradient Descent Simulated Annealing..."
@@ -210,10 +271,9 @@ def GradientNoise(Func,Jac,gamma,amp,tol,x_init,itmax,K,it_init=None, \
 
     min_it=it_init
 
-    DoF = range(N)
+    # Save the DOF
     if DirDoF!=None:
-        for i in DirDoF:
-            DoF.remove(i)
+        SavedDOF = x[DirDoF]
 
     print "Initial Gradient = ", np.linalg.norm(Jac(x_init)), "Initial Energy = ", Energy
     for j in range(K):
@@ -224,14 +284,15 @@ def GradientNoise(Func,Jac,gamma,amp,tol,x_init,itmax,K,it_init=None, \
             dt = 1.0/np.log(float(it+2))**0.5
             sigma =amp*np.sqrt(gamma/(np.log(it+2)*dt))*1.0/float(it+2)
             b = Jac(x)
-            new_x = x - dt/gamma*b + sigma*np.sqrt(dt)/gamma*rd.normal(0.0,1.0,N)
+            new_x = x - dt/gamma*b + sigma*np.sqrt(dt)/gamma*rd.normal(0.0,1.0,x.shape)
 
             if (bounds!=None and len(bounds)==2 ):
-                for i in DoF:
-                    if (new_x[i]>bounds[0] and new_x[i]<bounds[1]):
-                        x[i]=new_x[i]
+                choice = [new_x>bounds[0],new_x<bounds[1]]
+                x=np.select(choice,[new_x,x])
             else:
-                x = np.copy(new_x)
+                x = new_x
+            if DirDoF!=None:
+                x[DirDoF]=SavedDOF
 
             it+=1
             PrevEnergy = Energy
