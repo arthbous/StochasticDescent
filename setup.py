f36d6c94403b670fc83098e90bd5a5954a4b50b3
Arthur Bousquet <arth.bousquet@gmail.com>
2018-03-13 15:24:55 -0400

    updated setup.py

--- setup.py
+++ setup.py
@@ -3,10 +3,10 @@ from setuptools import setup
 
 setup(name='stdsct',
       version='1.0',
-      description='Edge-Averaged Finite Elements (EAFE) for FENiCS',
+      description='Stochastic Descent Algorithms',
       url='https://github.com/arthbous/StochasticDescent',
       author='Arthur Bousquet, Qipin Chen, Shuonan Wu',
-      author_email='',
+      author_email='arth.bousquet@gmail.com',
       license='GNU GENERAL PUBLIC LICENSE',
       install_requires=['numpy','scipy'],
       packages=['stdsct'],
